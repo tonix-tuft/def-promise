1236aa7e8129ea6d250143c9f8d0c8de44470cb0
Anton Bagdatyev (Tonix) <antonytuft@gmail.com>
2020-05-01 13:15:02 +0200

    Added isSettled, hasResolved and hasRejected methods; resolve and reject become methods defined on DefPromise prototype

--- src/index.js
+++ src/index.js
@@ -23,15 +23,61 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
+/**
+ * @type {WeakMap}
+ */
+const privateScope = new WeakMap();
+
 export default class DefPromise extends Promise {
   constructor(executor) {
     let resolve, reject;
+    let isSettled = false,
+      hasResolved = false,
+      hasRejected = false;
     super((resolveFunc, rejectFunc) => {
-      resolve = resolveFunc;
-      reject = rejectFunc;
-      executor(resolveFunc, rejectFunc);
+      resolve = (...args) => {
+        if (!isSettled) {
+          isSettled = true;
+          hasResolved = true;
+        }
+        resolveFunc(...args);
+      };
+      reject = (...args) => {
+        if (!isSettled) {
+          isSettled = true;
+          hasRejected = true;
+        }
+        rejectFunc(...args);
+      };
+      executor(resolve, reject);
     });
-    this.resolve = resolve;
-    this.reject = reject;
+    const priv = {
+      resolve,
+      reject,
+      isSettled: () => isSettled,
+      hasResolved: () => hasResolved,
+      hasRejected: () => hasRejected,
+    };
+    privateScope.set(this, priv);
+  }
+
+  resolve(...args) {
+    return privateScope.get(this).resolve(...args);
+  }
+
+  reject(...args) {
+    return privateScope.get(this).reject(...args);
+  }
+
+  isSettled() {
+    return privateScope.get(this).isSettled();
+  }
+
+  hasResolved() {
+    return privateScope.get(this).hasResolved();
+  }
+
+  hasRejected() {
+    return privateScope.get(this).hasRejected();
   }
 }
